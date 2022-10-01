ae8554a0eea0d4c5ee76e7be51353ce3e5c5fa71
ikenwaa <ajohn9353@gmail.com>
2022-10-01 17:43:13 +0100

    Updated main.go

--- main.go
+++ main.go
@@ -3,11 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	var agency = "LASRMA"
-	var appName = "Ticket Booking System"
-	const seatsPerTrip = 200
-	var remTickets = 200
+	var agency string = "LASRMA"
+	var appName string = "Ticket Booking System"
+	const seatsPerTrip int = 200
+	var remTickets int = 200
 
-	fmt.Printf("Welcome to %s %s - Purchase and book your train tickets here.\n", agency, appName)
-	fmt.Printf("There are %d seats available and %d tickets left for sale. Book yours now!\n", seatsPerTrip, remTickets)
+	fmt.Printf("Welcome to %v %v - Purchase and book your train tickets here.\n", agency, appName)
+	fmt.Printf("There are %v seats available and %v tickets left for sale. Book yours now!\n", seatsPerTrip, remTickets)
+
+	// Get user info
+	var userName string
+	var ticketsBought int
+
+	fmt.Printf("Please enter your username: ")
+	fmt.Scanf("%v", &userName)
+	fmt.Printf("Welcome %v, how many tickets do you want to purchase? ", userName)
+	fmt.Scanf("%d", &ticketsBought)
+	fmt.Printf("Hi %v, you have bought %d ticket(s).\n", userName, ticketsBought)
 }

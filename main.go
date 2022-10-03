13b942fb2ec05f80dd90e723017f8729d82eb54d
ikenwaa <ajohn9353@gmail.com>
2022-10-03 17:00:02 +0100

    Updated main.go

--- main.go
+++ main.go
@@ -3,21 +3,37 @@ package main
 import "fmt"
 
 func main() {
-	var agency string = "LASRMA"
-	var appName string = "Ticket Booking System"
+	agency := "LASRMA"
+	appName := "Ticket Booking Application"
 	const seatsPerTrip int = 200
-	var remTickets int = 200
+	var remTickets uint = 200
+	var bookings []string
 
 	fmt.Printf("Welcome to %v %v - Purchase and book your train tickets here.\n", agency, appName)
 	fmt.Printf("There are %v seats available and %v tickets left for sale. Book yours now!\n", seatsPerTrip, remTickets)
 
 	// Get user info
-	var userName string
-	var ticketsBought int
+	var firstName string
+	var lastName string
+	var email string
+	var trainTickets uint
 
-	fmt.Printf("Please enter your username: ")
-	fmt.Scanf("%v", &userName)
-	fmt.Printf("Welcome %v, how many tickets do you want to purchase? ", userName)
-	fmt.Scanf("%d", &ticketsBought)
-	fmt.Printf("Hi %v, you have bought %d ticket(s).\n", userName, ticketsBought)
+	fmt.Printf("Please enter your first name: \n")
+	fmt.Scan(&firstName)
+	fmt.Printf("Please enter your last name: \n")
+	fmt.Scan(&lastName)
+	fmt.Printf("Please enter your email address: \n")
+	fmt.Scan(&email)
+	fmt.Printf("Welcome %v, how many tickets do you want to buy? \n", firstName)
+	fmt.Scan(&trainTickets)
+	fmt.Printf("Hi %v, you have bought %d ticket(s). You will receive a confirmation email at %v.\n", firstName, trainTickets, email)
+
+	remTickets -= trainTickets
+	bookings = append(bookings, firstName + " " + lastName)
+	
+	fmt.Printf("Number of train tickets left is: %v\n", remTickets)
+	fmt.Printf("Type of bookings: %T\n", bookings)
+	fmt.Printf("First element: %v\n", bookings[0])
+	fmt.Println(bookings)
+	fmt.Printf("Length: %v\n", len(bookings))
 }
